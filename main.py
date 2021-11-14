f3528264c4e646d05831f6395fd04a87cddd162a
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-11-14 12:24:19 +0530

    Forgot

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from pyrogram.types import (
     InlineKeyboardMarkup
 )
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-API_ID = int(os.environ.get('API_ID'))
-API_HASH = os.environ.get('API_HASH'))
+BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
+API_ID = int(os.environ.get('API_ID', 12345))
+API_HASH = os.environ.get('API_HASH', "")
 
 print('[INFO] INITIALIZING CLIENT')
 crush = shashank(
